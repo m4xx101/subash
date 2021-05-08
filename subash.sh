d5d6f8aaadec8989ad02ab2f79ac9f9887977abd
D Ξ Ξ P Λ K <m4xx101101@gmail.com>
2021-05-08 15:25:27 +0530

    Update subash.sh

--- subash.sh
+++ subash.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+mkdir $1
+cd $1
+touch main
+
 domain=$1
-path_to_save=$2
+path_to_save=$(pwd)
 red=`tput setaf 1`
 green=`tput setaf 2`
 purple=`tput setaf 125`
@@ -20,25 +25,25 @@ banner()
 subdomain-recon()
 {
 banner "Finding Subdomains"
-curl -s "https://jldc.me/anubis/subdomains/"$domain | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" > $path_to_save
-assetfinder -subs-only $domain -silent | httpx -timeout 3 -threads 300 --follow-redirects -silent|anew $path_to_save|tee -a $passive
-curl -s "https://crt.sh/?q=%25."$domain"&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' |anew $path_to_save|tee -a $passive
-subfinder -d $domain|anew $path_to_save|tee -a $passive
-amass enum -d $domain -passive|anew $path_to_save|tee -a $passive
-findomain -t $domain -u $path_to_save-findomain.txt
-cat $path_to_save-findomain.txt |anew $path_to_save|tee -a $passive
-rm -f $path_to_save-findomain.txt
-echo ${green}"Done and results saved in"${reset} "==>" ${bold}${red}$path_to_save ${reset}
-tmp_var=$(cat $path_to_save|wc -l)
+curl -s "https://jldc.me/anubis/subdomains/"$domain | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" >> main
+assetfinder -subs-only $domain -silent | httpx -timeout 3 -threads 300 --follow-redirects -silent|anew main >> main
+curl -s "https://crt.sh/?q=%25."$domain"&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' |anew main >> main
+subfinder -d $domain|anew main >> main
+amass enum -d $domain -passive|anew main >> main
+findomain -t $domain -u main-findomain.txt
+cat main-findomain.txt |anew main >> main
+rm -f main-findomain.txt
+echo ${green}"Done and results saved in"${reset} "==>" ${bold}${red} main ${reset}
+tmp_var=$(cat main|wc -l)
 echo ""
 echo "Total" ${bold}${green}$tmp_var ${reset}"Subdomains Found"
 echo ""
 banner "Running httprobe on found list of subdomain"
 sleep 1
-cat $path_to_save |httprobe > $path_to_save-httprobed.txt
+cat main |httprobe > main-httprobed.txt
 echo ""
 sleep 1
-tmp_var2=$(cat $path_to_save-httprobed.txt|wc -l)
+tmp_var2=$(cat main-httprobed.txt|wc -l)
 echo ${bold}${green}$tmp_var2 ${reset}"Subdomains are alive out of${reset}"${bold}${green} $tmp_var${reset}
 end_time=`date +%s`
 duration=$(expr $end_time - $start_time)
